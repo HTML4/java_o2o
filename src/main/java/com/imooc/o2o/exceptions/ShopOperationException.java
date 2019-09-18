ae7181bd59f99f611b98b03333be6c23dc6bac40
林广杰 <378036288@qq.com>
2019-09-18 18:13:02 +0800

    4.6完

--- src/main/java/com/imooc/o2o/exceptions/ShopOperationException.java
+++ src/main/java/com/imooc/o2o/exceptions/ShopOperationException.java
@@ -4,6 +4,7 @@ package com.imooc.o2o.exceptions;
  * Created by jason on 2019/9/10.
  */
 public class ShopOperationException extends RuntimeException {
+    private static final long serialVersionUID = 0L;
     public ShopOperationException(String msg) {
         super(msg);
     }
